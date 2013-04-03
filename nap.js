a922ce08a48874eafc350b88cee274df379484a7
napster99 <morrisonleisure@163.com>
2013-04-03 22:28:49 +0800

    nap.html nap.js

---
+++ nap.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+try{
+
+	var str = fs.readFile('nap.html','utf-8',function(err,data){
+		http.createServer(function(req, res){
+			res.writeHead(200,{'Content-Type':'text/html'});
+			res.end(data);
+		}).listen(8124);
+	});
+
+	
+
+
+}catch(e){
+	alert(e);
+}
+
+
+console.log('Server running at http://127.0.0.1:8124/');
